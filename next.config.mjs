acdefece0236dfc626728353179f5cd765877af0
shaktiranjanmahanta <shakti003009@gmail.com>
2024-12-02 15:54:26 +0530

    Nessary changes to setup nextra library

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import nextra from "nextra";
+
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+const nextConfig = {
+  // Your Next.js configuration options go here
+  reactStrictMode: true, // Example option
+};
+
+export default withNextra(nextConfig);
